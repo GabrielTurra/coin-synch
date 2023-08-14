3e1a7e0c8b05f556963b29cf9476cf032cb017bc
Gabriel Turra <gabriel.turra@outlook.com>
2023-08-14 07:00:01 -0300

    refactor(components): remove example component

--- src/components/@example/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { ExampleComponent } from "./Example.styles";
-import { ExampleProps } from "./Example.types";
-
-export const Example: React.FC<ExampleProps> = () => {
-  return <ExampleComponent></ExampleComponent>;
-};
