8b6a8bd2c2110c696ab3149a5ca5c14e4e2dd0d1
Gabriel Turra <gabriel.turra@outlook.com>
2023-08-14 06:58:51 -0300

    build(lint): disable eslint on next build

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: [
-    "page.tsx",
-    "api.ts",
-    "api.tsx",
-  ],
+  pageExtensions: ["page.tsx", "api.ts", "api.tsx"],
   images: {
     domains: ["s2.coinmarketcap.com", "avatars.githubusercontent.com"],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
